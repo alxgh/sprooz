0a8512b5b69f6d733d5e45e47a0461a0c24e5ce3
MrAlexGh <mm.gholamian@yahoo.com>
2018-01-07 23:15:51 +0330

    :beer: Initial commit
    Initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: false,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "dist/css/main.css": "src/less/main.less" // destination file and source file
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['src/less/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less', 'watch']);
+};
---
+++ README.md
@@ -0,0 +1,18 @@
+# A simple rtl css framework
+
+Hello. nothing much to show for now just a grid system.
+
+Any help is appreciated :)
+
+## Compile
+
+> there is a compiled version in `dist` folder.
+>
+> Examples are in `html` folder.
+
+```bash
+git clone alxgh/sprooz sprooz
+cd sprooz
+npm install
+grunt
+```
